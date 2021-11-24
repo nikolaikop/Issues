741e1deeaa03a3a0d248f4436fa73e33045fd3be
Nikolai Pustovoit <nikolaikop@gmail.com>
2021-11-24 13:04:34 -0700

    FileOpenManager version 1

--- src/main/java/ru/netology/manager/FileOpenManager.java
+++ src/main/java/ru/netology/manager/FileOpenManager.java
@@ -1,2 +1,38 @@
-package ru.netology.manager;public class FileOpenManager {
+package ru.netology.manager;
+
+import java.util.*;
+
+public class FileOpenManager {
+
+    Map<String, String> map = new HashMap();
+
+    public void save(String key, String element) {
+        map.put(key, element);
+    }
+
+
+    public Map<String, String> registerNewApp(String key, String element) {
+        map.put(key, element);
+        return map;
+    }
+
+    public String getAppToOpenFile(String key) {
+        String element = map.get(key);
+        return element;
+    }
+
+    public Map<String, String> deleteApps(String key) {
+        map.remove(key);
+        return map;
+    }
+
+    public Set<String> getListOfAllRegisteredApps() {
+        Set<String> keySet = map.keySet();
+        return keySet;
+    }
+
+    public ArrayList<String> getListOfAllApps() {
+        ArrayList<String> value = new ArrayList<>(map.values());
+        return value;
+    }
 }
--- src/test/java/ru/netology/manager/FileOpenManagerTest.java
+++ src/test/java/ru/netology/manager/FileOpenManagerTest.java
@@ -1,4 +1,61 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
 class FileOpenManagerTest {
-  
+
+    private FileOpenManager fileOpenManager = new FileOpenManager();
+
+    @BeforeEach
+    public void before() {
+        fileOpenManager.save("doc", "Word");
+        fileOpenManager.save("exe", "Executable");
+    }
+
+    @Test
+    public void registerNewApp() {
+        Map<String, String> mapT = new HashMap<>();
+        mapT.put("ipg", "ACDSee");
+        mapT.put("doc", "Word");
+        mapT.put("exe", "Executable");
+        Map expected = mapT;
+        Map actual = fileOpenManager.registerNewApp("ipg", "ACDSee");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getApplicationToOpenFile() {
+        String expected = "Word";
+        String actual = String.valueOf(fileOpenManager.getAppToOpenFile("doc"));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeTheBindingApplications() {
+        Map<String, String> mapT = new HashMap<>();
+        mapT.put("doc", "Word");
+        Map expected = mapT;
+        Map actual = fileOpenManager.deleteApps("exe");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getListAllRegisteredExtensions() {
+        Set<String> expected = Set.of("exe", "doc");
+        Set actual = fileOpenManager.getListOfAllRegisteredApps();
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void getListAllApp() {
+        List<String> expected = List.of("Executable", "Word");
+        ArrayList actual = fileOpenManager.getListOfAllApps();
+        assertEquals(expected, actual);
+    }
+
 }
